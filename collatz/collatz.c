8f48769665e0d042ebbd84c07906e92df7625986
matthewkim3 <113797965+matthewkim3@users.noreply.github.com>
2022-09-26 12:27:04 -0400

    Update collatz.c

--- collatz/collatz.c
+++ collatz/collatz.c
@@ -19,11 +19,41 @@ recursively, and collatzGoto is implemented with the goto statement.
 #include <assert.h> /* assert */
 
 
-int collatzGoto(long unsigned int n) {}
+int collatzGoto(long unsigned int n) {
+int counter=0;
+    while(1){
+        if(n==1){
+            goto poop;
+        }
+        if(n%2==0){
+            n=n/2;
+            counter++;
+        }else{
+            n=n*3+1;
+            counter++;
+        }
+        
+    }
+    poop: return counter;
+}
 
-int collatzWhile(long unsigned int n) {}
+int collatzWhile(long unsigned int n) {
+    int counter=0;
+    while(n!=1){
+        if(n%2==0){
+            n=n/2;
+            counter++;
+        }else{
+            n=n*3+1;
+            counter++;
+        }
+    }
+    return counter;
+}
 
-int collatzRecursive(long unsigned int n) {}
+
+int collatzRecursive(long unsigned int n) {
+}
 
 
 int main () {
