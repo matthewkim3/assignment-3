ada0d0b54f24bc599fcae9cd84c5d76339b98cb0
matthewkim3 <113797965+matthewkim3@users.noreply.github.com>
2022-09-28 22:06:29 -0400

    Update complex.c

--- complex/complex.c
+++ complex/complex.c
@@ -9,6 +9,8 @@ compute the real part, a*c - b*d with RMULT(a, b, c, d) and the imaginary
 part, a*d + b*c with IMULT(a, b, c, d)
 */
 #include <assert.h>
+#define RMULT(a,b,c,d) a*c-b*d
+#define IMULT(a,b,c,d) a*d+b*c
 
 int main() {
     assert(RMULT(1, 2, 3, 4) == -5);
