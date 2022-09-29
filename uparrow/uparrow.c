92bf830ce46f0e12e1a86b89fd12df9725982414
matthewkim3 <113797965+matthewkim3@users.noreply.github.com>
2022-09-29 10:12:53 -0400

    Update uparrow.c

--- uparrow/uparrow.c
+++ uparrow/uparrow.c
@@ -36,7 +36,11 @@ types, do not use void.
 
 #include <assert.h> /* assert */
 
-void uparrow(void a, void b, void n) {}
+long long int uparrow(int a, int b, int n) {
+  if (n == 1) return pow(a, b);
+  else if (n>1&&b==0)return 1;
+  else return uparrow(a, uparrow(a, (b-1), n), n-1);
+}
 
 int main () {
     assert(uparrow(1, 1, 0) == 1);
